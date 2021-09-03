856b8702fcde671bae4c7c8c5dacacfbe5a961b3
Kubakubikula <jakub.zient@gmail.com>
2021-09-03 21:20:45 +0100

    corona study

---
+++ corona-virus.py
@@ -0,0 +1,16 @@
+corona-virus - research
+
+----
+The Pfizer/BioNtech 
+Comirnaty vaccine was listed for WHO Emergency Use Listing (EUL) 
+on 
+31 December 2020. 
+
+The SII/Covishield and AstraZeneca/AZD1222 vaccines 
+(developed by AstraZeneca/Oxford and manufactured by the State Institute of India and SK Bio respectively) were given EUL on 16 February. 
+
+The Janssen/Ad26.COV 2.S developed by Johnson & Johnson, was listed for EUL on 12 March 2021. 
+
+The Moderna COVID-19 vaccine (mRNA 1273) was listed for EUL on 30 April 2021 and the Sinopharm COVID-19 vaccine was listed for EUL on 7 May 2021. 
+
+The Sinopharm vaccine is produced by Beijing Bio-Institute of Biological Products Co Ltd, subsidiary of China National Biotec Group (CNBG). The Sinovac-CoronaVac was listed for EUL on 1 June 2021.
