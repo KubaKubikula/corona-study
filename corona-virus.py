16d54b3992d4dbd536dfcac7e21663b5bf8f0335
Kubakubikula <jakub.zient@gmail.com>
2021-09-04 12:12:43 +0100

    some writings

--- corona-virus.py
+++ corona-virus.py
@@ -9,8 +9,16 @@ on
 The SII/Covishield and AstraZeneca/AZD1222 vaccines 
 (developed by AstraZeneca/Oxford and manufactured by the State Institute of India and SK Bio respectively) were given EUL on 16 February. 
 
-The Janssen/Ad26.COV 2.S developed by Johnson & Johnson, was listed for EUL on 12 March 2021. 
+-- The Janssen/Ad26.COV 2.S developed by Johnson & Johnson, was listed for EUL on 12 March 2021. 
+--- viral vector vaccine
+	Viral vector vaccines use a modified version of one virus as a vector to deliver to a cell a nucleic acid coding for an antigen for another infectious agent. Viral vector vaccines do not cause infection with either the virus used as the vector, or the source of the antigen. The genetic material it delivers does not integrate into a person's genome.[1]
+	Viral vector vaccines enable antigen expression within cells and induce a robust cytotoxic T cell response, unlike subunit vaccines which only confer humoral immunity. Most viral vectors are designed to be incapable of replication because the necessary genes are removed.[2]
 
-The Moderna COVID-19 vaccine (mRNA 1273) was listed for EUL on 30 April 2021 and the Sinopharm COVID-19 vaccine was listed for EUL on 7 May 2021. 
+The Moderna COVID-19 vaccine (mRNA 1273) was listed for EUL on 30 A+ and the Sinopharm COVID-19 vaccine was listed for EUL on 7 May 2021. 
 
 The Sinopharm vaccine is produced by Beijing Bio-Institute of Biological Products Co Ltd, subsidiary of China National Biotec Group (CNBG). The Sinovac-CoronaVac was listed for EUL on 1 June 2021.
+
+
+Porto
+-----
+https://www.porto.pt/en/news/porto-built-a-large-scale-vaccination-programme-but-it-needs-vaccines-in-place
